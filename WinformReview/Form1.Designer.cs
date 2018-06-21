03facd95472e1df9d46f55dfdeb9f49d709a2aa7
Trung Đào <daoquangtrung1998@outlook.com.vn>
2018-06-22 01:12:12 +0700

    Add project files.

---
+++ WinformReview/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WinformReview
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxEmp = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridViewProject = new System.Windows.Forms.DataGridView();
+            this.buttonSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProject)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select an employee:";
+            // 
+            // comboBoxEmp
+            // 
+            this.comboBoxEmp.FormattingEnabled = true;
+            this.comboBoxEmp.Location = new System.Drawing.Point(178, 12);
+            this.comboBoxEmp.Name = "comboBoxEmp";
+            this.comboBoxEmp.Size = new System.Drawing.Size(200, 24);
+            this.comboBoxEmp.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Select projects:";
+            // 
+            // dataGridViewProject
+            // 
+            this.dataGridViewProject.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewProject.Location = new System.Drawing.Point(16, 82);
+            this.dataGridViewProject.Name = "dataGridViewProject";
+            this.dataGridViewProject.RowTemplate.Height = 24;
+            this.dataGridViewProject.Size = new System.Drawing.Size(554, 200);
+            this.dataGridViewProject.TabIndex = 3;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(495, 311);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 30);
+            this.buttonSave.TabIndex = 4;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 353);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.dataGridViewProject);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBoxEmp);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProject)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxEmp;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridViewProject;
+        private System.Windows.Forms.Button buttonSave;
+    }
+}
+
---
+++ WinformReview/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinformReview
+{
+    public partial class Form1 : Form
+    {
+        string connectionStr = ConfigurationManager.ConnectionStrings["ProjectDB"].ConnectionString;
+        public Form1()
+        {
+            InitializeComponent();
+            string selectQuery = "SELECT * FROM EmployeeTBL";
+            SqlDataAdapter adapter = new SqlDataAdapter(selectQuery, connectionStr);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            comboBoxEmp.DataSource = dt;
+            comboBoxEmp.DisplayMember = "EmployeeName";
+            comboBoxEmp.ValueMember = "EmployeeID";
+            comboBoxEmp.SelectedIndexChanged += ComboBoxEmp_SelectedIndexChanged;
+            ComboBoxEmp_SelectedIndexChanged(null, null);
+        }
+
+        private void ComboBoxEmp_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int employeeID = int.Parse(comboBoxEmp.SelectedValue.ToString());
+            string query = string.Format("select [ProjectID], [ProjectName] from Projects where ProjectID not in (select ProjectID from ProjectResourceTBL where EmployeeID = {0})", employeeID);
+            SqlDataAdapter adapter = new SqlDataAdapter(query, connectionStr);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridViewProject.DataSource = dt;
+            //resize column name
+            dataGridViewProject.Columns[1].Width = dataGridViewProject.Columns[1].GetPreferredWidth(DataGridViewAutoSizeColumnMode.AllCells, true);
+            DataGridViewCheckBoxColumn c = new DataGridViewCheckBoxColumn();
+            c.HeaderText = "Available Projects";
+            c.Name = "JoinProject";
+            c.TrueValue = true;
+            c.FalseValue = false;
+            dataGridViewProject.Columns.Insert(2, c);
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = null; 
+            //for (int i = 0; i < dataGridViewProject.Rows.Count; i++)
+            //{
+            //    DataGridViewRow row = dataGridViewProject.Rows[i];
+            //    object obj = row.Cells["JoinProject"].Value;
+            //    if (obj != null && obj.ToString().ToLower().Equals("true"))
+            //    {
+            //        string id = row.Cells["ProjectID"].Value.ToString();
+            //        string name = row.Cells["ProjectName"].Value.ToString();
+            //        //do insertion
+            //        string insertQuery = "insert into ProjectResourceTBL values(@pid, @eid)";
+            //        SqlCommand cmd = new SqlCommand(insertQuery, conn);
+
+            //    }
+            //}
+            SqlTransaction transaction = null;
+            try
+            {
+                conn = new SqlConnection(connectionStr);
+                conn.Open();
+                transaction = conn.BeginTransaction();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                for (int i = 0; i < dataGridViewProject.Rows.Count; i++)
+                {
+                    DataGridViewRow row = dataGridViewProject.Rows[i];
+                    object obj = row.Cells["JoinProject"].Value;
+                    if (obj != null && obj.ToString().ToLower().Equals("true"))
+                    {
+                        string id = row.Cells["ProjectID"].Value.ToString();
+                        string name = row.Cells["ProjectName"].Value.ToString();
+                        cmd.CommandText += string.Format("INSERT INTO ProjectResourceTBL VALUES(@pid{0}, @eid{0});", i);
+                        cmd.Parameters.Add(string.Format("@pid{0}", i), SqlDbType.VarChar).Value = id;
+                        cmd.Parameters.Add(string.Format("@eid{0}", i), SqlDbType.Int).Value = int.Parse(comboBoxEmp.SelectedValue.ToString());
+                    }
+                }
+                Debug.WriteLine(cmd.CommandText);
+                cmd.Transaction = transaction;
+                cmd.ExecuteNonQuery();
+                transaction.Commit();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("ex: " + ex.Message);
+                Debug.WriteLine("msg: " + ex.StackTrace);
+                try
+                {
+                    transaction.Rollback();
+                    return;
+                }
+                catch (Exception exRollback)
+                {
+                    MessageBox.Show(exRollback.StackTrace, exRollback.Message, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            finally
+            {
+                if (conn.State != ConnectionState.Closed)
+                    conn.Close();
+                transaction.Dispose();
+            }
+            MessageBox.Show("Successfully updated");
+            ComboBoxEmp_SelectedIndexChanged(null, null);
+        }
+    }
+}
